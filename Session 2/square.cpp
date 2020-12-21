bb1bbe11193b038aa41ec2febf0c3644392efaa8
YingLiang Ma (ac7020) <ac7020@coventry.ac.uk>
2020-12-21 21:53:00 +0000

    add week 2

--- Session 2/square.cpp
+++ Session 2/square.cpp
@@ -3,7 +3,6 @@
 //
 // OpenGL program to draw a square.
 // 
-// Coventry Univ.
 ///////////////////////////////////
 
 #ifdef __APPLE__
@@ -17,76 +16,77 @@
 #endif
 
 /*
-		---- EXERCISE 1 OVERVIEW ----
-		FOR THIS EXERCISE WE ONLY NEED TO CREATE AN VERTEX BUFFER OBJECT (VBO) (AS LEGACY HAS THESE IN).
+		---- EXERCISE 2 OVERVIEW ----
+		FOR THIS EXERCISE AS WE HAVE 2 OBJECTS (THE SQUARE FROM EXECRISE 1 AND A TRIANGLE),
+		WE NEED TO USE VERTEX ARRAY OBJECTS (VAOs) FOR THIS.
+		THIS ALLOWS US TO SET THE BUFFERS FOR EACH ONCE AND THEN SWITCH BETWEEN THEM WHEN DRAWING
+		BY SELECTING THE VAO FOR THAT OBJECT.
+		
+		WE ONLY NEED TO CREATE A VAO (AS LEGACY HAS THESE IN).
 		THIS MEANS NO SHADERS NEED TO BE USED - THIS WILL COME LATER IN EXERCISE 3.
 
-		FOR A VBO, WE NEED TO PUT THE VERTEX DATA INTO A BUFFER AND THEN SEND THIS TO THE GPU.
-		THIS WILL BE DIFFERENT LATER WHEN WE COMBINE CO-ORDS AND COLOUR INTO ONE VERTEX STRUCT, 
-		BUT WE WANT TO EASE YOU INTO MODERN OPENGL WITH THIS WEEKS LAB TASKS.
+		FOR A VAO, WE WILL CREATE IT, DO THE VBO PARTS NEEDED FOR VERTEX/OBJECT CREATION IN THE SETUP FUNCTION
+		AND THEN SWITCH BETWEEN THESE IN THE DRAWING FUNCTION.
 
-		---- WHAT IS A VBO ----
-		A VERTEX BUFFER OBJECT OR VBO IS AN ARRAY OF DATA THAT IS COPIED TO THE GPU BEFORE DRAWING AND RAN ON THE GPU
+		---- WHAT IS A VAO ----
 
-		FLOWCHART FOR VBO USAGE IN THIS EXAMPLE:
+		A VERTEX ARRAY OBJECT OR VAO REMEMBERS ALL VBOS AND MEMORY LAYOUTS OF THEM. IT ALLOWS SWITCHING BETWEEN DIFFERENT VERTEX DATA AND VERTEX FORMATS VIA BINDING A VAO.
+
+
+		FLOWCHART FOR VAO USAGE IN THIS EXAMPLE:
 
 		---- SETUP FUNCTION ----
-		GENERATE A (1) BUFFER OBJECT NAME																	(https://www.khronos.org/registry/OpenGL-Refpages/es2.0/xhtml/glGenBuffers.xml)
-		(IN THIS CASE) TELL OPENGL TO ENABLE THE USE OF 2 CAPABILITIES, -VERTEX ARRAY- AND -COLOR ARRAY-	(https://www.khronos.org/registry/OpenGL-Refpages/gl2.1/xhtml/glEnableClientState.xml)
-			THESE ALLOW US TO USE GLDRAWARRAYS LATER
 
-		BIND THE BUFFER OBJECT NAME - AKA TELL OPENGL THIS IS THE BUFFER THE BELOW COMMANDS WILL WORK ON	(https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glBindBuffer.xhtml)
-		CREATE AND INITIALIZE INTO THE BUFFER'S DATA STORE (BUFFER TYPE, SIZE, DATA GOING IN, USAGE)		(https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glBufferData.xhtml)
-			(IN THIS CASE) NO DATA GOES IN STRAIGHT AWAY BECAUSE OF THE BELOW SPLITTING OF DATA HENCE THE null
+		GENERATE VERTEX BUFFER OBJECT NAMES FOR EACH OBJECT (2 OVERALL - 1 FOR EACH)						(https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glGenVertexArrays.xhtml)
+		GENERATE BUFFER OBJECT NAMES FOR EACH OBJECT (2 OVERALL - 1 FOR EACH)								(https://www.khronos.org/registry/OpenGL-Refpages/es2.0/xhtml/glGenBuffers.xml)
+		
+		BIND THE FIRST VAO FOR THE FIRST OBJECT																(https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glBindVertexArray.xhtml)
+			(IN THIS CASE) SQUARE
 
-		(IN THIS CASE) TELL OPENGL THAT PART (FIRST HALF) OF THE BUFFER (BUFFERSUBDATA) IS FOR VERTICES		(https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glBufferSubData.xhtml)
-			AND PLACE THE VERTEX DATA IN THAT HALF
-		(IN THIS CASE) TELL OPENGL THAT PART (SECOND HALF) OF THE BUFFER (BUFFERSUBDATA) IS FOR COLOURS		(https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glBufferSubData.xhtml)
-			AND PLACE THE COLOUR DATA IN THAT HALF
+		DO THE VBO THINGS FROM EXERICSE 1 FOR THE SQUARE
 
-		(IN THIS CASE) TELL OPENGL HOW MANY THINGS MAKE UP A VERTEX POSITION AND WHERE THIS STARTS			(https://www.khronos.org/registry/OpenGL-Refpages/gl2.1/xhtml/glVertexPointer.xml)
-		(IN THIS CASE) TELL OPENGL HOW MANY THINGS MAKE UP A VERTEX COLOUR AND WHERE THIS STARTS			(https://www.khronos.org/registry/OpenGL-Refpages/gl2.1/xhtml/glColorPointer.xml)
-			TAKING INTO ACCOUNT THAT COLOURS ARE IN THE SECOND HALF OF THE BUFFER
+		BIND THE SECOND VAO FOR THE SECOND OBJECT															(https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glBindVertexArray.xhtml)
+			(IN THIS CASE) TRIANGLE
+
+		DO THE VBO THINGS FROM EXERICSE 1 FOR THE TRIANGLE
+
 
 		---- DRAW FUNCTION ----
-		IN DRAW SCENE, DRAW A TRIANGLE STRIP OF 4 VERTICES EACH FRAME										(https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glDrawArrays.xhtml)
-			BASED ON THE ABOVE BUFFER AND THE DATA WE PLACED INTO IT			
-		
-		---- WHY DO WE NEED TO DO THIS? ----
-		THE BUFFER WILL LOOK LIKE THIS WHEN FILLED WITH DATA (WITHOUT THE SPACES - BUT SO WE CAN SEE THIS)
-		[ xyz xyz xyz xyz rgb rgb rgb rgb ]
 
-		WE THEN TELL OPENGL WHAT THAT DATA MEANS AS WE (AS HUMANS) SEE HOW IT FITS BUT ALL OPENGL SEES ARE NUMBERS ALL TOGETHER SUCH AS
+		BIND THE FIRST VAO FOR THE FIRST OBJECT																(https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glBindVertexArray.xhtml)
+			(IN THIS CASE) SQUARE
 
-		[20.0,20.0,0.0,80.0,20.0,0.0,20.0,80.0,0.0,80.0,80.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+		DRAW A TRIANGLE STRIP OF 4 VERTICES EACH FRAME														(https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glDrawArrays.xhtml)
+			BASED ON THE BUFFER IN VAO 0 AND THE DATA WE PLACED INTO IT			
 
-		WITH glVertexPointer WE TELL OPENGL THAT A VERTEX POSITION IS 3 FLOATS AND TO START FROM THE BEGINNING OF THE BUFFER
-		WITH glColorPointer WE TELL OPENGL THAT A VERTEX COLOUR IS ALSO 3 FLOATS BUT TO START FROM AFTER THE VERTICES IN THE BUFFER (BY GIVING THE SIZE OF ALL THE VERTICES AS A STRIDE)
+		BIND THE SECOND VAO FOR THE SECOND OBJECT															(https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glBindVertexArray.xhtml)
+			(IN THIS CASE) TRIANGLE
 
-		WHEN WE DRAW OUR SHAPE WE TELL OPENGL THERE ARE 4 VERTICES SO IT KNOWS AFTER THE FIRST 4 LOTS OF 3 FLOATS THAT IT IS NO LONGER POSITIONS AND NOW IS ONTO COLOURS FOR THE NEXT 4 LOTS OF 3 FLOATS
-		
-		OPENGL THEN WOULD SEE THE DATA LIKE THIS
-
-		[	20.0,20.0,0.0,	80.0,20.0,0.0,	20.0,80.0,0.0,	80.0,80.0,0.0,	0.0,0.0,0.0,	0.0,0.0,0.0,	0.0,0.0,0.0,	0.0,0.0,0.0]
-			VERT 1 POS		VERT 2 POS		VERT 3 POS		VERT 4 POS		VERT 1 COLOUR	VERT 2 COLOUR	VERT 3 COLOUR	VERT 4 COLOUR
-			3 FLOATS		3 FLOATS		3 FLOATS		3 FLOATS		3 FLOATS		3 FLOATS		3 FLOATS		3 FLOATS
-			~~~~~~~~~~~~~~~~~~~~~~~~~~~~~VERTEX POSITION DATA ~~~~~~~~~~~   ~~~~~~~~~~~~~~~~~~~~~~~~~~~VERTEX COLOUR DATA ~~~~~~~~~~~~~
+		DRAW A TRIANGLE OF 3 VERTICES EACH FRAME															(https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glDrawArrays.xhtml)
+			BASED ON THE BUFFER IN VAO 1 AND THE DATA WE PLACED INTO IT
 */
 
 /*
 	MODERN OPENGL - STORE VERTICES ON GPU
 
-	4 vertices =
+	SQUARE => 4 vertices =
 
 	4 positions ( 3 floats each )
 	4 colours	( 3 floats each )
+
+	TRIANGLE => 3 vertices =
+
+	3 positions ( 3 floats each )
+	3 colours	( 3 floats each )
 */
+
 float vertices[] = 
 {
 	20.0, 20.0, 0.0,
 	80.0, 20.0, 0.0,
 	20.0, 80.0, 0.0,
 	80.0, 80.0, 0.0
+	
 };
 
 float colours[] = 
@@ -97,17 +97,41 @@ float colours[] =
 	0.0, 0.0, 0.0
 };
 
-/// MODERN OPENGL - WILL HOLD 1 BUFFER OBJECT NAME
-unsigned int buffer[1];
+float vertices1[] =
+{
+	30.0, 30.0, 1.0,
+	70.0, 30.0, 1.0,
+	30.0, 70.0, 1.0
+
+};
+
+float colours1[] =
+{
+	1.0, 0.0, 1.0,
+	1.0, 0.0, 1.0,
+	1.0, 0.0, 1.0
+};
+
+/// MODERN OPENGL - WILL HOLD 2 BUFFER OBJECT NAMES (SQUARE, TRIANGLE)
+unsigned int buffer[2];
+/// MODERN OPENGL - WILL HOLD 2  (SQUARE, TRIANGLE)
+unsigned int vao[2]; // Array of VAO ids.
 
 // Drawing routine.
 void drawScene(void)
 {
    glClear(GL_COLOR_BUFFER_BIT);
 
+   /// MODERN OPENGL - BIND THE FIRST VAO FOR THE FIRST OBJECT
+   glBindVertexArray(vao[0]);
    /// MODERN OPENGL - DRAW A GL_TRIANGLE_STRIP USING THE 4 VERTICES IN THE VBO
    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
    
+   /// MODERN OPENGL - BIND THE FIRST VAO FOR THE FIRST OBJECT
+   glBindVertexArray(vao[1]);
+   /// MODERN OPENGL - DRAW A GL_TRIANGLE_STRIP USING THE 4 VERTICES IN THE VBO
+   glDrawArrays(GL_TRIANGLES, 0, 3);
+
    /*
 			LEGACY OPENGL - CANNOT USE ANYMORE
    
@@ -129,20 +153,22 @@ void drawScene(void)
 void setup(void) 
 {
    glClearColor(1.0, 1.0, 1.0, 0.0); 
+   /// MODERN OPENGL -GENERATE VERTEX BUFFER OBJECT NAMES FOR EACH OBJECT(2 OVERALL - 1 FOR EACH)
+   glGenVertexArrays(2, vao); // Generate VAO ids
+   
+   /// MODERN OPENGL -GENERATE BUFFER OBJECT NAMES FOR EACH OBJECT(2 OVERALL - 1 FOR EACH)
+   glGenBuffers(2, buffer); // Generate buffer ids.
 
-   /// MODERN OPENGL - GENERATE A(1) BUFFER OBJECT NAME
-   glGenBuffers(1, buffer); // Generate buffer ids.
+   /// MODERN OPENGL - BIND THE FIRST VAO FOR THE FIRST OBJECT
+   glBindVertexArray(vao[0]);
 
-   /// MODERN OPENGL - (IN THIS CASE) TELL OPENGL TO ENABLE THE USE OF 2 CAPABILITIES, - VERTEX ARRAY - AND - COLOR ARRAY -
    glEnableClientState(GL_VERTEX_ARRAY);// Enable two vertex arrays: co-ordinates and color.
    glEnableClientState(GL_COLOR_ARRAY);
-
 
    /// MODERN OPENGL - BIND THE BUFFER OBJECT NAME - AKA TELL OPENGL THIS IS THE BUFFER THE BELOW COMMANDS WILL WORK ON
    glBindBuffer(GL_ARRAY_BUFFER, buffer[0]);   // Bind vertex buffer 
    /// MODERN OPENGL - CREATE AND INITIALIZE INTO THE BUFFER'S DATA STORE (BUFFER TYPE, SIZE, DATA GOING IN, USAGE)	
    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) + sizeof(colours), NULL, GL_STATIC_DRAW); // and reserve space
-
   
    /// MODERN OPENGL - 	(IN THIS CASE) TELL OPENGL THAT PART(FIRST HALF) OF THE BUFFER(BUFFERSUBDATA) IS FOR VERTICES
    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);  // Copy vertex coordinates data into first half of vertex buffer.
@@ -154,6 +180,18 @@ void setup(void)
    glVertexPointer(3, GL_FLOAT, 0, 0);  // Specify vertex and color pointers to the start of the respective data.
    /// MODERN OPENGL - 			(IN THIS CASE) TELL OPENGL HOW MANY THINGS MAKE UP A VERTEX COLOUR AND WHERE THIS STARTS
    glColorPointer(3, GL_FLOAT, 0, (GLvoid*)(sizeof(vertices)));
+
+   glBindVertexArray(vao[1]);
+
+   glEnableClientState(GL_VERTEX_ARRAY);// Enable two vertex arrays: co-ordinates and color.
+   glEnableClientState(GL_COLOR_ARRAY);
+
+   glBindBuffer(GL_ARRAY_BUFFER, buffer[1]);   // Bind vertex buffer 
+   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1) + sizeof(colours1), NULL, GL_STATIC_DRAW); // and reserve space																					
+   glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices1), vertices1);  // Copy vertex coordinates data into first half of vertex buffer.
+   glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices1), sizeof(colours1), colours1); // Copy vertex color data into second half of vertex buffer.							
+   glVertexPointer(3, GL_FLOAT, 0, 0);  // Specify vertex and color pointers to the start of the respective data.
+   glColorPointer(3, GL_FLOAT, 0, (GLvoid*)(sizeof(vertices1)));
 }
 
 // OpenGL window reshape routine.
